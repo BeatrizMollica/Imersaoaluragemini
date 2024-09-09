821186d9dc9934a54830dfb4801e9d081e47d06a
BeatrizMollica <biamollica@gmail.com>
2024-09-09 18:44:04 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, é uma skatista profissional que conquistou o mundo com seu talento precoce e estilo único. Aos poucos anos de idade, ela já coleciona títulos e inspira uma nova geração de skatistas.",
+        link: "https://www.instagram.com/rayssalealoficial/",
+        tags: "skate fadinha"
+    },
+
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta completa, com um talento inegável e uma determinação que a coloca entre as melhores do mundo. Sua história é um exemplo de superação e inspiração para todos aqueles que buscam alcançar seus objetivos.",
+        link: "https://www.instagram.com/rebecarandrade/?hl=pt-br",
+        tags: "ginastica ouro"
+    },
+
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica apurada e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é um dos grandes nomes do judô brasileiro.",
+        link: "https://www.instagram.com/_beatrizsouzaar/",
+        tags: "judo judoka "
+    }
+    
+]; 
+
